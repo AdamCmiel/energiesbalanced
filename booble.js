871a1a2a24438829fa6e5cc7a3c8575af0d82ef5
Adam Cmiel <adamcmiel@gmail.com>
2013-08-22 22:34:04 -0700

    Push index to heroku

--- booble.js
+++ booble.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.use(express.static(__dirname + '/public'));
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
