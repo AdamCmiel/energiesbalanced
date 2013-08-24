4278c7238ac33efb968fe9899afe2cc8f1c39b68
Adam Cmiel <adamcmiel@gmail.com>
2013-08-23 23:39:29 -0700

    debug attempt for including other js files

--- config/fblogin.js
+++ config/fblogin.js
@@ -39,8 +39,10 @@ app.get('/session', function(req, response){
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
 //     /auth/facebook/callback
-app.get('/auth/facebook', passport.authenticate('facebook'));
-
+app.get('/auth/facebook', fucntion(){
+  console.log('auth attempt');
+  passport.authenticate('facebook'));
+});
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
