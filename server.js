73a32e20ad18896d82e51621a0e492fcc5694b0c
Adam Cmiel <adamcmiel@gmail.com>
2013-08-23 22:57:09 -0700

    parse out fblogin code, rename server file

---
+++ server.js
@@ -0,0 +1,38 @@
+//app server js file
+
+//declare app
+var express = require("express");
+
+//required libraries
+var fs = require('fs');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var mongoose = require('mongoose');
+
+//Bootstrap db connection
+mongoose.connect(config.db);
+
+
+var app = express();
+
+//expose app
+exports = module.exports = app;
+
+app.configure(function() {
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: process.env.SUPER_SECRET_SESSIONS_KEY}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+});
+
+
+
+app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
