6eba8e6024be3f15ea40becb65441d25b96fb075
Adam Cmiel <adamcmiel@gmail.com>
2013-08-25 11:44:43 -0700

    remove fb module

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ var fs = require('fs');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var mongoose = require('mongoose');
+//in app models
 var Cat = require('./config/cat.js');
 
 //connect to the database
 mongoose.connect(process.env.MONGOHQ_URL);
 
-//var facelog = require('/config/fblogin.js');
-console.log(process.env.SUPER_SECRET_SESSIONS_KEY);
-
+//declare app variable
 var app = express();
 
 app.configure(function() {
@@ -29,15 +28,64 @@ app.configure(function() {
 });
 
 
+
+//cat module
+app.post('/cats', Cat.create);
+
+
+//facebook logins
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://energiesbalanced.herokuapp.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null, profile);
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+//facebook login module
+ 
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  // Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed. 
+  passport.authenticate('facebook', { 
+    successRedirect: '/',
+    failureRedirect: '/' 
+  })
+);
+
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
+//sessions relay
+app.get('/session', function(req, response){
+  response.send({session: req.session});
+});
+
 app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-app.post('/cats', Cat.create);
-
 
 //expose app
 exports = module.exports = app;
