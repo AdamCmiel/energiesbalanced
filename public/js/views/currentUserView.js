e1e1360ca28bc85dddd942ad998e5592682ab862
Adam Cmiel <adamcmiel@gmail.com>
2013-11-20 12:16:33 -0800

    adding alerts for testing CUView

--- public/js/views/currentUserView.js
+++ public/js/views/currentUserView.js
@@ -9,9 +9,11 @@ define([
   var CurrentUserView = Backbone.View.extend({
   	el: $('#currentUser'),
   	initialize: function(){
+      alert('initialize CU view');
   		this.render();
   	},
   	render: function(){
+      alert('render CU view');
   		if(this.model && this.model.facebook_id){
   			this.$el.html(this.template(this.model.toJSON()));
   		};
