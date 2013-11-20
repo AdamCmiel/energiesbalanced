7de8cf57c217525879f9e30162f0cf0448193714
Adam Cmiel <adamcmiel@gmail.com>
2013-11-20 15:27:02 -0800

    add daily schedule view

--- public/js/routers/router.js
+++ public/js/routers/router.js
@@ -11,27 +11,42 @@ define([
                 Backbone.history.start({pushState: true})
             },
             routes:{
-                        "/": "checkUser",
-                        "sign_in": "loadSignIn",
-                        "nav": "loadNav",
-                        "logout": "logout"
+                "/": "checkUser",
+                "sign_in": "loadSignIn",
+                "nav": "loadNav",
+                "logout": "logout",
+                "nav/schedule": "renderSchedule"
             },
             checkUser: SessionsController.checkUser(),
             loadSignIn: function(){
             	SessionsController.loggedIn();
                 $('body').children().remove();
                 $('body').html($('#signIn').html());
+                Backbone.history.navigate('nav', {trigger:true});
             },
             loadNav: function(){
-            		//SessionsController.navLoggedIn();
-                    //$('.container').children().remove();
-                    $('header').html($('#signedInHeaderTemplate').html());
-                    SessionsController.renderUser();
-                    $('.container').html($('#navTemplate').html());
+            	//SessionsController.navLoggedIn();
+                //$('.container').children().remove();
+                $('header').html($('#signedInHeaderTemplate').html());
+                
+                //put this back in
+                //SessionsController.renderUser();
+                $('.container').html($('#navTemplate').html());
+                $('li').on('click', 'a', function(e){
+                    e.preventDefault();
+                    Backbone.history.navigate('nav/'+$(e.target).attr('href'), {trigger:true});
+                });
+                $('header').on('click', 'a', function(e){
+                    e.preventDefault();
+                    Backbone.history.navigate('/', {trigger: true});
+                });
             },
             logout: function(){
-                    SessionsController.logout();
-                    this.navigate('/api/logout');
+                SessionsController.logout();
+                this.navigate('/api/logout');
+            },
+            renderSchedule: function(){
+                $('.container').html($('#scheduleTemplate').html());
             }
     }));
 	return router;
